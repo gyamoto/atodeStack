67948af781b672f6adc976b1a3577475a31fa342
gyamoto <farundorlnightray@gmail.com>
2015-11-01 23:56:03 +0900

    add OPEN and DELETE

--- background.js
+++ background.js
@@ -1,30 +1,71 @@
+var command_open = "open:",
+    command_save = "save:",
+    command_delete = "delete:";
+
 chrome.omnibox.onInputChanged.addListener(function(text, suggest) {
   console.log('inputChanged: ' + text);
 
-  var suggest = function(storage) {
+  var suggestion = function(storage) {
+    console.log(storage);
+
     var stacks = storage.stacks;
     var suggests = [];
     stacks.forEach(function(stack) {
       if(isSuggest(text, stack)) {
-        suggests.push(createStack(url));
+        suggests.push(createOpenSuggest(stack));
+        suggests.push(createDeleteSuggest(stack));
       }
     })
     suggest(suggests);
   }
+  getStack(suggestion);
 });
 
 chrome.omnibox.onInputEntered.addListener(function(text) {
   console.log('inputEntered: ' + text);
-  saveStack(text);
+
+  if(text.indexOf(command_open) != -1) {
+    openStack(sliceUrl(text, command_open));
+  } else if(text.indexOf(command_delete) != -1) {
+    deleteStack(sliceUrl(text, command_delete));
+  } else {
+    saveStack(sliceUrl(text, command_save));
+  }
 });
 
+function openStack(url) {
+  var stack = createStack(url);
+  chrome.tabs.create({url: stack.url});
+}
+
 function saveStack(url) {
-  var get = function(storage) {
+  var save = function(storage) {
+    console.log(storage);
+
     var stack = createStack(url);
-    storage.stacks.push(stack);
+    var index = indexOfStack(storage.stacks, stack);
+    if(index != -1) {
+      storage.stacks[index] = stack;
+    } else {
+      storage.stacks.push(stack);
+    }
     chrome.storage.sync.set({"stacks": storage.stacks}, function() {});
   }
-  getStack(get);
+  getStack(save);
+}
+
+function deleteStack(url) {
+  var del = function(storage) {
+    console.log(storage);
+
+    var stack = createStack(url);
+    var index = indexOfStack(storage.stacks, stack);
+    if(index != -1) {
+      storage.stacks.splice(index, 1);
+      chrome.storage.sync.set({"stacks": storage.stacks}, function() {});
+    }
+  }
+  getStack(del);
 }
 
 function getStack(onGetStack) {
@@ -32,19 +73,20 @@ function getStack(onGetStack) {
   chrome.storage.sync.get(first, onGetStack);
 }
 
-function Suggest() {
-  this.content = "";
-  this.description = "";
+function createOpenSuggest(stack) {
+  var content = command_open + stack.url;
+  var description = (stack.memo == "")
+    ? content
+    : '"' + stack.memo + '"' + content;
+  return {content: content, description: description};
 }
 
-function createSuggest(url) {
-  var suggest = new suggest();
-  suggest.description = url;
-  if (includeMemo(url)) {
-    suggest.content = sliceMemo(url);
-    suggest.description = sliceUrl(url);
-  }
-  return suggest;
+function createDeleteSuggest(stack) {
+  var content = command_delete + stack.url;
+  var description = (stack.memo == "")
+    ? content
+    : '"' + stack.memo + '"' + content;
+  return {content: content, description: description};
 }
 
 function isSuggest(text, stack) {
@@ -61,22 +103,50 @@ function Stack() {
 
 function createStack(url) {
   var stack = new Stack();
-  stack.url = url;
   if (includeMemo(url)) {
-    stack.url = sliceMemo(url);
-    stack.memo = sliceUrl(url);
+    stack.url = sliceMemoUrl(url);
+    stack.memo = sliceMemo(url);
+  } else {
+    stack.url = url;
   }
   return stack;
 }
 
 function includeMemo(url) {
-  return (url.match(/:/g) || []).length > 0;
+  return (url.split(":").length > 1);
+}
+
+function indexOfStack(stacks, target) {
+  var index = -1;
+  stacks.forEach(function(stack, i) {
+    if(stack.url == target.url) {
+      index = i;
+    }
+  });
+  return index;
 }
 
 function sliceMemo(url) {
-  return url.slice(0, url.indexOf(":"));
+  if(url.split(":").length > 2) {
+    return url.slice(0, url.indexOf(":"));
+  } else {
+    return "";
+  }
 }
 
-function sliceUrl(url) {
-  return url.slice(url.indexOf(":") + 1);
-} 
+function sliceMemoUrl(url) {
+  if(url.split(":").length > 2) {
+    return url.slice(url.indexOf(":") + 1);
+  } else {
+    return url;
+  }
+}
+
+function sliceUrl(url, target) {
+  if(url.split(target).length > 1) {
+    return url.slice(url.indexOf(target) + target.length);
+  } else {
+    return url;
+  }
+  
+}
